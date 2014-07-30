366269c36bc85d72b7da97b49e5c9409e8813bb0
adsl8212 <adsl8212@gmail.com>
2014-07-31 02:28:52 +0800

    後台機制

---
+++ test1/demo/app/controllers/groups_controller.rb
@@ -0,0 +1,64 @@
+class GroupsController < ApplicationController
+	before_action :authenticate_user!, :only => [:new, :edit, :create, :update, :destroy] 
+	def index
+		@groups = Group.all
+	end
+	def show
+		@group = Group.find(params[:id])
+		@posts = @group.posts
+	end
+	def new
+		@group = Group.new
+	end
+	def create
+		@group = current_user.groups.build(group_params)
+
+		if @group.save
+			current_user.join!(@group)
+			redirect_to groups_path
+		else
+			render :new
+		end
+	end
+
+	def update
+		@group = current_user.groups.find(params[:id])
+		if @group.update(group_params)
+			redirect_to group_path(@group)
+		else
+			render :edit
+		end
+	end
+
+	def edit
+		@group = current_user.groups.find(params[:id])
+	end
+
+	def destroy
+		@group = current_user.groups.find(params[:id])
+		@group.destroy
+		redirect_to groups_path
+	end 
+
+	def join
+		@group = Group.find(params[:id])
+
+		if !current_user.is_member_of?(@group)
+			current_user.join!(@group)
+		end
+		redirect_to groups_path(@group)
+	end
+
+	def quit
+		@group = Group.find(params[:id])
+		if current_user.is_member_of?(@group)
+			current_user.quit!(@group)
+		end
+		redirect_to groups_path(@group)
+	end
+
+	private
+		def group_params
+			params.require(:group).permit(:title, :description)
+		end
+end
